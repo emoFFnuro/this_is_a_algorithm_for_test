cacbc54c2fe8b801868ab78585b77cb6c3cf08f9
emoFFnuro <62496713+emoFFnuro@users.noreply.github.com>
2021-02-01 19:29:16 +0900

    Add files via upload

---
+++ 2293.py
@@ -0,0 +1,17 @@
+## BOJ.2293 -> https://www.acmicpc.net/problem/2293
+## same with kanpsack problem..
+import sys
+n, k = map(int,sys.stdin.readline().split())
+
+coin = []
+for _ in range(n):
+    coin.append(int(sys.stdin.readline()))
+
+dp = [0] * (k+1)
+dp[0] = 1
+for c in coin:
+    for i in range(1,k+1):
+        if c <= i:
+            dp[i] += dp[i-c]
+
+print(dp[k])
