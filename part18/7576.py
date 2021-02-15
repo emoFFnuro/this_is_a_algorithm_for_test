a3cb76cebb9351cf42b40bf7437a23d575cb36e8
emoFFnuro <62496713+emoFFnuro@users.noreply.github.com>
2021-02-15 19:21:53 +0900

    Create 7576.py

---
+++ part18/7576.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+r = sys.stdin.readline
+
+
+def bfs(M, N, box):
+    dx = [0, 0, 1, -1]
+    dy = [-1, 1, 0, 0]
+
+    days = -1
+
+    while line:
+        days += 1
+        for _ in range(len(ripe)):
+            x, y = ripe.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < N and 0 <= ny < M and box[nx][ny] == 0):
+                    box[nx][ny] = box[x][y] + 1
+                    line.append([nx, ny])
+
+    for b in box:
+        if 0 in b:
+            return -1
+    return days
+
+
+M, N = map(int, r().split())
+box, line = [], deque()
+for i in range(N):
+    row = list(map(int, r().split()))
+    for j in range(M):
+        if row[j] == 1:
+            line.append([i, j])
+    box.append(row)
+
+print(bfs(M, N, box))
